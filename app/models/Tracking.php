3dc728c576b72e4a37833b70d41eaf8a58de60c9
Rogerio Benco <rogerio.benco@infracommerce.com.br>
2024-05-02 12:04:56 -0300

    Clean code to Cooltracker Api

--- app/models/Tracking.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Tracking extends Eloquent{
-    protected $table = 'rastreio';
-    protected $softDelete = true;
-}
-
--- app/models/TrackingHistory.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class TrackingHistory extends Eloquent{
-    protected $table = 'rastreio_historico';
-}
-
